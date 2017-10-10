9c3c4401f3a638304ecb9406b7305e018396a230
Rostradamus <hyungro@hotmail.com>
2017-10-10 16:38:55 -0700

    added post_list.js

---
+++ static/js/post_list.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rolee on 2017-10-10.
+ */
+$(document).ready(function () {
+    sortTable();
+    console.log("hi");
+
+});
+
+function sortTable() {
+    var table = document.getElementById("post_table");
+    console.log("I found the table!")
+}
